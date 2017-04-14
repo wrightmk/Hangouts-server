17a56c1e0dd530ba21ef2e57c095b94a1436e845
Mitchell Wright <wrightmitchell@hotmail.com>
2017-04-13 17:42:08 -0700

    deployment ready take 2

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-// require '../../.gitignore';
+
 import DB_KEY from './app_keys'
 
 // console.log(DB_KEY);
@@ -8,6 +8,7 @@ export default () => {
   mongoose.Promise = global.Promise;
 
   mongoose.connect(`${DB_KEY}`);
+  // mongoose.connect(`mongodb://localhost/awesome-meetup`);
   mongoose.connection
   .once('open', () => console.log('mongodb is running'))
   .on('error', err => console.error(err))
