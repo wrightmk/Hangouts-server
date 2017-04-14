a1a3c2e18e7523b0b89eff9b67522504a952b982
Mitchell Wright <wrightmitchell@hotmail.com>
2017-04-13 18:04:31 -0700

    changed nodemon to node in script pacakge.json

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose';
 
-import DB_KEY from './app_keys'
+// import DB_KEY from './app_keys'
 
 // console.log(DB_KEY);
 
 export default () => {
   mongoose.Promise = global.Promise;
 
-  mongoose.connect(`${DB_KEY}`);
+  mongoose.connect(process.env['DB_KEY']);
   // mongoose.connect(`mongodb://localhost/awesome-meetup`);
   mongoose.connection
   .once('open', () => console.log('mongodb is running'))
